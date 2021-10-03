6c1d1927d1a78d2f83c02a12716391d47c1e93bc
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-10-03 15:49:19 -0300

    fixes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 import './config/database/connect';
 import routes from './routes';
 import path from 'path';
