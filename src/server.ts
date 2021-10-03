e34ca9c1e40e4a0085f19f69bc0aeef834094c1f
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-10-03 15:00:13 -0300

    small adjustments

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,23 @@ import cors from 'cors';
 import './config/database/connect';
 import routes from './routes';
 import path from 'path';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const app = express();
-const port = 3335;
+const port = process.env.PORT || 3335;
 
-
-mongoose.connect(`mongodb+srv://RafaelCouto:macbook.00@cluster0.ypakl.mongodb.net/fast-remedy?retryWrites=true&w=majority`, {
- useNewUrlParser: true,
- useUnifiedTopology: true
-})
-
+mongoose.connect(
+	`mongodb+srv://RafaelCouto:macbook.00@cluster0.ypakl.mongodb.net/fast-remedy?retryWrites=true&w=majority`,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
 
 app.use(express.json());
-app.use('/file', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')))
+app.use('/file', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
 app.use(cors());
 app.use(routes);
 
-app.listen(process.env.PORT || port, () => {
-    console.log('Server Running');
+app.listen(port, () => {
+	console.log(`Server Running on port ${port}`);
 });
