743aa4c83e90d328a106020eec2a5c32a5080afa
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-10-16 12:47:11 -0300

    Removed token expiration

--- src/app/utils/generateToken.ts
+++ src/app/utils/generateToken.ts
@@ -1,9 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 function generateToken(params = {}) {
-	const token = jwt.sign(params, `${process.env.SECRET}`, {
-		expiresIn: 86400,
-	});
+	const token = jwt.sign(params, `${process.env.SECRET}`);
 
 	return token;
 }
