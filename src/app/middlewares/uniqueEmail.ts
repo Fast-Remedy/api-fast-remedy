5aa96e98a31d4ccff9d033990ee284b5c7988f0c
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-10-22 13:36:51 -0300

    Fixes in email validation

--- src/app/middlewares/uniqueEmail.ts
+++ src/app/middlewares/uniqueEmail.ts
@@ -2,6 +2,7 @@ import CustomersModel from '../models/CustomersModel';
 import StoresModel from '../models/StoresModel';
 
 export async function uniqueEmailCustomer(req, res, next) {
+	const { id } = req.params;
 	const { emailCustomer } = req.body;
 
 	const hasEmail = await CustomersModel.findOne({ emailCustomer });
@@ -9,7 +10,9 @@ export async function uniqueEmailCustomer(req, res, next) {
 	console.log(hasEmail);
 
 	if (hasEmail) {
-		return res.status(409).json({ error: 'Esse email já está cadastrado.' });
+		if (String(hasEmail._id) !== String(id)) {
+			return res.status(409).json({ error: 'Esse email já está cadastrado.' });
+		}
 	}
 
 	next();
@@ -21,11 +24,10 @@ export async function uniqueEmailStore(req, res, next) {
 
 	const hasEmail = await StoresModel.findOne({ emailStore });
 
-	console.log(hasEmail._id);
-	console.log(id);
-
 	if (hasEmail) {
-		return res.status(409).json({ error: 'Esse email já está cadastrado.' });
+		if (String(hasEmail._id) !== String(id)) {
+			return res.status(409).json({ error: 'Esse email já está cadastrado.' });
+		}
 	}
 
 	next();
